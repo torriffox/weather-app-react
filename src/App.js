89a5ebd713a14fedb5a7ea92581699234eae1602
torriffox <torriffox@gmail.com>
2023-12-14 12:41:36 +0200

    [02] Added the GitHub repo and Netlify URL.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/torriffox" target="_blank">
+          Viktoriia Bril
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/torriffox/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a href="https://dainty-halva-abb75a.netlify.app/" target="_blank">
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
