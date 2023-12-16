1ea62678c0d3db7ea4c544c03659f1423549b09c
torriffox <torriffox@gmail.com>
2023-12-16 12:19:10 +0200

    [05] Added formatted date on the weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,51 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./Weather.css";
+import FormattedDate from "./FormattedDate.js";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              className="form-control"
-              type="search"
-              placeholder="Enter a city .."
-              autoFocus="on"
-            />
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+
+  function handleResponse(response) {
+    setWeatherData({
+      city: response.data.city,
+      country: response.data.country,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      img: response.data.condition.icon_url,
+      temperature: response.data.temperature.current,
+      feels_like: response.data.temperature.feels_like,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+    });
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Enter a city .."
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                className="btn btn-primary w-100"
+                type="submit"
+                value="Search"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              className="btn btn-primary w-100"
-              type="submit"
-              value="Search"
+        </form>
+        <h1>
+          {weatherData.city}, {weatherData.country}
+        </h1>
+        <ul>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <img
+              className="float-left"
+              src={weatherData.img}
+              alt="Mostly cloudy"
             />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">°C</span>
           </div>
-        </div>
-      </form>
-      <h1>New York, USA</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img
-            className="float-left"
-            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-            alt="Mostly cloudy"
-          />
-          <span className="temperature">5</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 10%</li>
-            <li>Humidity: 55%</li>
-            <li>Wind: 3 km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Feels like: {Math.round(weatherData.feels_like)} °C</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "ac49tfd4a3a68b207d8do5734e42e190";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading ...";
+  }
 }
