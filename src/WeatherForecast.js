c341553b085d9904ed2b896b91af03b8dc7d68e7
torriffox <torriffox@gmail.com>
2023-12-24 17:21:28 +0200

    [12] Created refresh forecast.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,15 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.data.city]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
